c37c07ebab85dc6ad1c8f9f2fc842afbdfbd792f
cesar7816 <e-cecortes@gentera.com.mx>
2022-01-26 23:59:44 -0600

    testing tf SA

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 resource "google_compute_address" "ip_address" {
   name = "my-address"
 }
+resource "google_service_account" "service_account" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
+}
