37053f6a5a5c0c5e65007b57847e688728473546
cesar7816 <e-cecortes@gentera.com.mx>
2022-01-27 12:18:08 -0600

    sa-jenkins2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_address" "ip_address" {
-  name = "my-address-1"
+  name = "my-address-2"
 }
 resource "google_service_account" "service_account" {
   account_id   = "service-account-id-1"
